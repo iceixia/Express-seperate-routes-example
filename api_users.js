f72025cdef2b21a9c57fcebaf070c66227a2e2aa
dan <dan.jones.94@hotmail.com>
2016-02-15 03:48:50 +0000

    example

---
+++ api_users.js
@@ -0,0 +1,15 @@
+exports.find = function(req, res){
+    res.send("DEBUG TEST");
+};
+
+exports.create = function(req, res){
+    
+};
+
+exports.edit = function(req, res){
+    
+};
+
+exports.delete = function(req,res){
+    
+};
--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 var express = require('express');
 var app = express();
 
+var api_media = require('./api_media');
+var api_authentication = require('./api_authentication');
+var api_users = require('./api_users');
+var api_upload = require('./api_upload');
+
+module.exports = app;
+
 app.use('/html', express.static(__dirname + "/static/html"));
 app.use('/css', express.static(__dirname + "/static/css"));
 app.use('/js', express.static(__dirname + "/static/js"));
@@ -22,6 +29,12 @@ app.get('/admin', function(req, res){
 
 app.get('/test', function(req, res){
     res.send("It works!");
-})
+});
+
+app.get('/users', api_users.find);
+app.patch('/users', api_users.edit);
+app.delete('/users', api_users.delete);
+app.post('/users', api_users.create);
+
 app.listen(8000);
 console.log("Server started");
